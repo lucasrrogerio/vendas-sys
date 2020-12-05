3b430a1d2ff4c633624ca0539ae4515099a26c14
Luisa Esposito <a.luisa.esposito@gmail.com>
2020-12-05 01:56:42 -0300

    Adiciona swagger api doc

--- src/main/java/br/com/uff/vendasys/web/controller/ProdutoController.java
+++ src/main/java/br/com/uff/vendasys/web/controller/ProdutoController.java
@@ -3,19 +3,27 @@ package br.com.uff.vendasys.web.controller;
 import br.com.uff.vendasys.domain.entity.Produto;
 import br.com.uff.vendasys.service.ProdutoService;
 import br.com.uff.vendasys.web.dto.ProdutoDTO;
+import br.com.uff.vendasys.web.dto.UsuarioDTO;
 import br.com.uff.vendasys.web.mapper.MapperUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("produto")
+@Tag(name = "Produto")
 public class ProdutoController {
 
     @Autowired
@@ -23,54 +31,100 @@ public class ProdutoController {
 
     MapperUtil mapperUtil = MapperUtil.getInstance();
 
+    @Operation(summary = "Salva um produto novo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Produto criado com sucesso",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoDTO.class)) })
+    })
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ProdutoDTO salvarProduto(@RequestBody ProdutoDTO produtoDTO) {
         Produto produto = mapperUtil.mapTo(produtoDTO, Produto.class);
         Produto salvo = produtoService.salvarProduto(produto);
         return mapperUtil.mapTo(salvo, ProdutoDTO.class);
     }
 
+    @Operation(summary = "Busca um produto por seu id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto encontrado",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Produto nao encontrado", content = @Content)
+    })
     @GetMapping("{id}")
-    public ProdutoDTO buscarPorId(@PathVariable Long id) {
+    public ProdutoDTO buscarPorId(@Parameter(description = "id de produto a ser encontrado") @PathVariable Long id) {
         Produto produto = produtoService.buscarPorId(id);
         if (Objects.isNull(produto))
             throw new ResponseStatusException(HttpStatus.NO_CONTENT);
         return mapperUtil.mapTo(produto, ProdutoDTO.class);
     }
 
+    @Operation(summary = "Retorna todos os produtos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produtos encontrados",
+                    content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ProdutoDTO.class))) })
+    })
     @GetMapping
     public List<ProdutoDTO> buscarTodos() {
         return mapperUtil.toList(produtoService.buscarTodos(), ProdutoDTO.class);
     }
 
+    @Operation(summary = "Retorna todos os produtos ativos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produtos ativos encontrados",
+                    content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ProdutoDTO.class))) })
+    })
     @GetMapping("ativos")
     public List<ProdutoDTO> buscarAtivos() {
         return mapperUtil.toList(produtoService.buscarAtivos(), ProdutoDTO.class);
     }
 
+    @Operation(summary = "Busca produtos por categira")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produtos de uma categoria encontrados",
+                    content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ProdutoDTO.class))) })
+    })
     @GetMapping("categoria/{cod}")
-    public List<ProdutoDTO> buscarPorCategoria(@PathVariable String cod) {
+    public List<ProdutoDTO> buscarPorCategoria(@Parameter(description = "codigo da categoria de produtos")@PathVariable String cod) {
         return mapperUtil.toList(produtoService.buscarPorCategoria(cod), ProdutoDTO.class);
     }
 
+    @Operation(summary = "Altera um produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto alterado com sucesso",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Produto nao encontrado", content = @Content)
+    })
     @PutMapping("{id}")
-    public ProdutoDTO alterarProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+    public ProdutoDTO alterarProduto(@Parameter(description = "id de produto a ser encontrado") @PathVariable Long id,
+                                     @RequestBody ProdutoDTO produtoDTO) {
         Produto produto = produtoService.alterarProduto(id, mapperUtil.mapTo(produtoDTO, Produto.class));
         if (Objects.isNull(produto))
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado");
         return mapperUtil.mapTo(produto, ProdutoDTO.class);
     }
 
+    @Operation(summary = "Inativa um produto buscando por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto inativado com sucesso",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Produto nao encontrado", content = @Content)
+    })
     @PutMapping("inativar/{id}")
-    public ProdutoDTO inativar(@PathVariable Long id) {
+    public ProdutoDTO inativar(@Parameter(description = "id de produto a ser inativado") @PathVariable Long id) {
         Produto produto = produtoService.inativarProduto(id);
         if (Objects.isNull(produto))
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado");
         return mapperUtil.mapTo(produto, ProdutoDTO.class);
     }
 
+    @Operation(summary = "Remove um produto por seu id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Produto removido com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto nao encontrado", content = @Content)
+    })
     @DeleteMapping("{id}")
-    public void remover(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@Parameter(description = "id de produto a ser removido") @PathVariable Long id) {
         if (Objects.isNull(produtoService.buscarPorId(id)))
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto nao encontrado");
         produtoService.removerProduto(id);
--- src/main/java/br/com/uff/vendasys/web/controller/UsuarioController.java
+++ src/main/java/br/com/uff/vendasys/web/controller/UsuarioController.java
@@ -4,7 +4,14 @@ import br.com.uff.vendasys.domain.entity.Usuario;
 import br.com.uff.vendasys.service.UsuarioService;
 import br.com.uff.vendasys.web.dto.UsuarioDTO;
 import br.com.uff.vendasys.web.mapper.MapperUtil;
-import org.modelmapper.ModelMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +22,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("usuario")
+@Tag(name = "Usuario")
 public class UsuarioController {
 
     @Autowired
@@ -22,6 +30,12 @@ public class UsuarioController {
 
     MapperUtil mapperUtil = MapperUtil.getInstance();
 
+    @Operation(summary = "Cria um novo usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuario criado com sucesso",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Usuario invalido", content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UsuarioDTO criarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
@@ -29,28 +43,52 @@ public class UsuarioController {
         return mapperUtil.mapTo(usuarioService.salvarUsuario(usuario), UsuarioDTO.class);
     }
 
-    @GetMapping("{id}")
-    public UsuarioDTO buscarPorId(@PathVariable Long id) {
+    @Operation(summary = "Busca um usuario por seu id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado", content = @Content)
+    })
+    @GetMapping("/{id}")
+    public UsuarioDTO buscarPorId(@Parameter(description = "id do usuario a ser buscado") @PathVariable Long id) {
         Usuario usuario = usuarioService.buscarPorId(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario nao encontrado"));
         return mapperUtil.mapTo(usuario, UsuarioDTO.class);
     }
 
+    @Operation(summary = "Busca usuarios do tipo vendedor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuarios encontrados",
+                    content = { @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UsuarioDTO.class))) })
+    })
     @GetMapping("vendedores")
     public List<UsuarioDTO> buscarTodosVendedores() {
         return mapperUtil.toList(usuarioService.buscarTodosVendedores(), UsuarioDTO.class);
     }
 
+    @Operation(summary = "Altera um usuario existente")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado", content = @Content)
+    })
     @PutMapping("{id}")
-    public UsuarioDTO alterarUsuario(@PathVariable Long id, @RequestBody UsuarioDTO usuarioDTO) {
+    public UsuarioDTO alterarUsuario(@Parameter(description = "id de usuario a ser alterado") @PathVariable Long id,
+                                     @RequestBody UsuarioDTO usuarioDTO) {
         Usuario usuario = usuarioService.alterarUsuario(id, mapperUtil.mapTo(usuarioDTO, Usuario.class));
         if (Objects.isNull(usuario))
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario nao encontrado");
         return mapperUtil.mapTo(usuario, UsuarioDTO.class);
     }
 
+    @Operation(summary = "Remove um usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuario removido", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado", content = @Content)
+    })
     @DeleteMapping("{id}")
-    public void remover(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@Parameter(description = "id de usuario a ser removido") @PathVariable Long id) {
         if (usuarioService.buscarPorId(id).isEmpty())
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario nao encontrado");
         usuarioService.removerUsuario(id);
